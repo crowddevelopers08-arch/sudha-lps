7e1d6e118892d812aed84643b6599c4b70487cae
crowddevelopers08-arch <crowddevelopers08@gmail.com>
2026-05-08 14:56:52 +0530

    sudha new lp acne treatmentds

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const geistSans = Geist({
 const outfit = Outfit({
   subsets: ["latin"],
   variable: "--font-outfit",
-})
+});
 
 export const metadata: Metadata = {
   title: "Sudha Aesthetics",
@@ -44,17 +44,28 @@ export default function RootLayout({
       className={`${geistSans.variable} ${outfit.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        {/* ✅ GTM Noscript Fallback (for users without JavaScript) */}
+        {/* ✅ GTM Noscript Fallback */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-TGW8KZJT"
             height="0"
             width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
+            style={{ display: "none", visibility: "hidden" }}
           />
         </noscript>
 
-        {/* ✅ Google Tag Manager Main Script */}
+        {/* ✅ Meta Pixel Noscript */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=769869192756511&ev=PageView&noscript=1"
+            alt="facebook-pixel"
+          />
+        </noscript>
+
+        {/* ✅ Google Tag Manager */}
         <Script id="gtm" strategy="afterInteractive">
           {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -71,6 +82,7 @@ export default function RootLayout({
           src="https://www.googletagmanager.com/gtag/js?id=AW-18095694782"
           strategy="afterInteractive"
         />
+
         <Script id="google-ads" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
@@ -79,7 +91,30 @@ export default function RootLayout({
             gtag('config', 'AW-18095694782');
           `}
         </Script>
-        
+
+        {/* ✅ Meta Pixel Code */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;
+            n.push=n.loaded=!0;
+            n.version='2.0';
+            n.queue=[];
+            t=b.createElement(e);
+            t.async=!0;
+            t.src=v;
+            s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}
+            (window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '769869192756511');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
         <ScrollRevealInit />
         {children}
       </body>
